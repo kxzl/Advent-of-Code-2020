fb260bb59d5d1c31e784caf3925a2ef00e36218f
kxzl <roman@slobodchikov.net>
2020-12-09 20:35:33 +0100

    Dag 9 part 1

---
+++ D91.ps1
@@ -0,0 +1,29 @@
+﻿#$MinListe = Get-Content -Path ".\D9_Input\test.txt"
+$MinListe = Get-Content -Path ".\D9_Input\Input.txt"
+
+$preamble = 25
+
+:outer for ($i = $preamble; $i -lt $MinListe.Length; $i++) {
+
+    $p_list = @()
+    $p_list += $MinListe[($i-$preamble)..$i]
+    [int]$tallet = $MinListe[$i]
+    $check = $false
+
+    :inner for ($z = 0; $z -lt $p_list.Length; $z++ ) {
+        $subt = $tallet - $p_list[$z]
+        if ($p_list.Contains($subt)) {
+            $check = $true
+            break inner
+        }
+
+    }
+
+    if (!($check)) {
+        break outer
+    } 
+
+}  
+
+
+Write-Output "Taller er: $($MinListe[$i])"
